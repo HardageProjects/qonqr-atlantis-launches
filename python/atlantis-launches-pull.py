7b4b50c8076b0ecff6cd87a2e0b17e9e28cd6a7b
HardageProjects <129780464+HardageProjects@users.noreply.github.com>
2023-07-01 16:44:08 -0500

    fixing battle check

--- python/atlantis-launches-pull.py
+++ python/atlantis-launches-pull.py
@@ -28,8 +28,8 @@ for div_tag in parse.find_all('div',{'class':'col-md-12 col-xs-12'}):
     h1_tag = div_tag.find('h1')
     text_in_h1 = h1_tag.get_text(strip=True)
 
-check = text_in_h1.find("under way")
-if check < 0:
+check = text_in_h1.find(" over")
+if check > 0:
     print('No on-going battle at ' + timestamp)
     sys.exit()
     
