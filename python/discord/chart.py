75794dedda778f8f07f683acc2032a57444c09e3
HardageProjects <129780464+HardageProjects@users.noreply.github.com>
2023-07-05 15:04:05 -0500

    Update chart.py

--- python/discord/chart.py
+++ python/discord/chart.py
@@ -14,7 +14,6 @@ database = os.environ['DATABASE']
 user = os.environ['USER']
 password = os.environ['PASSWORD']
 discord_token = os.environ['DISCORD_TOKEN']
-channel_id = 556885461210103822
 
 ## Set up Discord bot
 intents = Intents.default()
@@ -26,39 +25,39 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name='chart')
 
 async def chart(ctx):
-    if ctx.channel.id == channel_id:
-        await ctx.message.delete()
+    # Create an engine that connects to the PostgreSQL server
+    engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{database}')
 
-        # Create an engine that connects to the PostgreSQL server
-        engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{database}')
+    # Define a SQL query to find the maximum existing ID value
+    query = 'SELECT FACTION, TIMESTAMP, LAUNCHES FROM qonqr.atlantis_launches WHERE DATE_TRUNC(\'month\',TIMESTAMP) = DATE_TRUNC(\'month\',CURRENT_DATE)'
+    # 'SELECT * FROM qonqr.atlantis_launches'
+    with engine.begin() as conn:
+        result = pd.read_sql_query(query, conn)
 
-        # Define a SQL query to find the maximum existing ID value
-        query = 'SELECT FACTION, TIMESTAMP, LAUNCHES FROM qonqr.atlantis_launches WHERE DATE_TRUNC(\'month\',TIMESTAMP) = DATE_TRUNC(\'month\',CURRENT_DATE)'
-        # 'SELECT * FROM qonqr.atlantis_launches'
-        with engine.begin() as conn:
-            result = pd.read_sql_query(query, conn)
+    # Convert timestamp to datetime
+    result['timestamp'] = pd.to_datetime(result['timestamp'])
 
-        # Convert timestamp to datetime
-        result['timestamp'] = pd.to_datetime(result['timestamp'])
+    # Group the data by faction and timestamp, then sum the launches within each group
+    grouped_data = result.groupby(['faction','timestamp'])['launches'].sum().reset_index()
 
-        # Group the data by faction and timestamp, then sum the launches within each group
-        grouped_data = result.groupby(['faction','timestamp'])['launches'].sum().reset_index()
+    # Pivot the data so that each faction has its own column
+    pivoted_data = grouped_data.pivot(index='timestamp', columns='faction', values='launches')
 
-        # Pivot the data so that each faction has its own column
-        pivoted_data = grouped_data.pivot(index='timestamp', columns='faction', values='launches')
+    chart = pivoted_data.plot(color=['purple','red','green'])
+    chart.set_title('Atlantis Launches Over Time')
+    chart.set_xlabel('Time')
+    chart.set_ylabel('Launch Count')
+    chart.legend(title='Faction')
+    chart.xaxis.set_major_formatter(mdates.DateFormatter('%B %d, %y %H:%M'))
 
-        chart = pivoted_data.plot(color=['purple','red','green'])
-        chart.set_title('Atlantis Launches Over Time')
-        chart.set_xlabel('Time')
-        chart.set_ylabel('Launch Count')
-        chart.legend(title='Faction')
-        chart.xaxis.set_major_formatter(mdates.DateFormatter('%B %d, %y %H:%M'))
+    image = chart.get_figure()
+    image.savefig('export.png')
 
-        image = chart.get_figure()
-        image.savefig('export.png')
+    ##plt.show()
+    with open('export.png', 'rb') as f:
+        await ctx.send(file=discord.File(f, 'export.png'))
 
-        ##plt.show()
-        with open('export.png', 'rb') as f:
-            await ctx.send(file=discord.File(f, 'export.png'))
+    ## Clear command
+    await ctx.message.delete()
 
 bot.run(discord_token)
